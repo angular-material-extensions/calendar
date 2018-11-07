acc993a940f68013c67e23b20b3cd12e29bf2739
Anthony Nahas <anthony.na@hotmail.de>
2018-11-07 07:05:33 +0100

    demo(app): disabled the outline of the buttons generated from bootstrap

--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import sdk from '@stackblitz/sdk';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
 
