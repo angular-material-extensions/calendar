2d7ce8cb93fbccbd0256b37926aeae331fa854be
Anthony Nahas <anthony.na@hotmail.de>
2018-11-12 11:35:27 +0100

    feat(package): added year input for MatCalendarMonthPerYearViewComponent

--- src/module/component/mat-calendar-yearly-view/mat-calendar-month-per-year-view/mat-calendar-month-per-year-view.component.ts
+++ src/module/component/mat-calendar-yearly-view/mat-calendar-month-per-year-view/mat-calendar-month-per-year-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, Input, OnInit, Optional, ViewEncapsulation} from '@angular/core';
+import {Component, Inject, Input, OnChanges, OnInit, Optional, SimpleChanges, ViewEncapsulation} from '@angular/core';
 import * as _moment from 'moment';
 import {MAT_DATE_FORMATS, MatCalendarCell, MatDateFormats, NativeDateAdapter} from '@angular/material';
 
@@ -10,10 +10,13 @@ const moment = _moment;
   styleUrls: ['./mat-calendar-month-per-year-view.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class MatCalendarMonthPerYearViewComponent implements OnInit {
+export class MatCalendarMonthPerYearViewComponent implements OnInit, OnChanges {
 
   @Input()
-  month: number = new Date().getMonth();
+  month: number;
+
+  @Input()
+  year: number;
 
   private _activeDate: Date;
 
@@ -30,16 +33,18 @@ export class MatCalendarMonthPerYearViewComponent implements OnInit {
   _weeks: MatCalendarCell[][];
 
   monthMatrix: [number, Array<number>] = [7, []];
-  currentDate: Date = new Date();
 
-  constructor(@Optional() @Inject(MAT_DATE_FORMATS) private _dateFormats: MatDateFormats,
+  selectedDate: Date = new Date();
+
+  constructor(@Optional() @Inject(MAT_DATE_FORMATS)
+              private _dateFormats: MatDateFormats,
               public nativeDateAdapter: NativeDateAdapter) {
 
     const firstDayOfWeek = this.nativeDateAdapter.getFirstDayOfWeek();
     const narrowWeekdays = this.nativeDateAdapter.getDayOfWeekNames('narrow');
     const longWeekdays = this.nativeDateAdapter.getDayOfWeekNames('long');
 
-    console.log('getDayOfWeekNames', this.nativeDateAdapter.getDayOfWeekNames('short'));
+    // console.log('getDayOfWeekNames', this.nativeDateAdapter.getDayOfWeekNames('short'));
     // Rotate the labels for days of the week based on the configured first day of the week.
     const weekdays = longWeekdays.map((long, i) => {
       return {long, narrow: narrowWeekdays[i]};
@@ -48,31 +53,31 @@ export class MatCalendarMonthPerYearViewComponent implements OnInit {
     this._weekdays = weekdays.slice(firstDayOfWeek).concat(weekdays.slice(0, firstDayOfWeek));
     this._todayDate = this._getDateInCurrentMonth(this.nativeDateAdapter.today());
 
-    console.log('this._weekdays = ', this._weekdays);
+    // console.log('this._weekdays = ', this._weekdays);
   }
 
   ngOnInit() {
-    console.log('input month = ', this.month);
-    this.currentDate.setMonth(this.month);
+    this.selectedDate.setMonth(this.month);
+    this.selectedDate.setFullYear(this.year);
+    this.init();
+    console.log('on init: ', this.selectedDate);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.year) {
+      this.year = changes.year.currentValue;
+    }
+  }
+
+  init() {
     this._todayDate = this._getDateInCurrentMonth(this.nativeDateAdapter.today());
-    const firstOfMonth = this.nativeDateAdapter.createDate(this.nativeDateAdapter.getYear(this.currentDate),
-      this.nativeDateAdapter.getMonth(this.currentDate), 1);
+    const firstOfMonth = this.nativeDateAdapter.createDate(this.nativeDateAdapter.getYear(this.selectedDate),
+      this.nativeDateAdapter.getMonth(this.selectedDate), 1);
     this._firstWeekOffset =
       (7 + this.nativeDateAdapter.getDayOfWeek(firstOfMonth) -
         this.nativeDateAdapter.getFirstDayOfWeek()) % 7;
     this._createWeekCells();
     // console.log(this._weeks);
-    this.fillTheMonthMatrix();
-  }
-
-  fillTheMonthMatrix(): void {
-
-    const start: any = moment().startOf('month');
-    const end: any = moment().endOf('month');
-
-    console.log('summary -> start is', start.toObject(), 'and the end is ', end.toObject());
-    // console.log(`summary -> start is ${start.weekday()} and the end is ${end.weekday()}`);
-    console.log(`summary -> start is ${start.toDate()} and the end is ${end.toDate()}`);
   }
 
   /**
@@ -80,7 +85,7 @@ export class MatCalendarMonthPerYearViewComponent implements OnInit {
    * Returns null if the given Date is in another month.
    */
   private _getDateInCurrentMonth(date: Date | null): number | null {
-    return date && this._hasSameMonthAndYear(date, this.currentDate) ?
+    return date && this._hasSameMonthAndYear(date, this.selectedDate) ?
       this.nativeDateAdapter.getDate(date) : null;
   }
 
@@ -92,7 +97,7 @@ export class MatCalendarMonthPerYearViewComponent implements OnInit {
 
   /** Creates MatCalendarCells for the dates in this month. */
   private _createWeekCells() {
-    const daysInMonth = this.nativeDateAdapter.getNumDaysInMonth(this.currentDate);
+    const daysInMonth = this.nativeDateAdapter.getNumDaysInMonth(this.selectedDate);
     const dateNames = this.nativeDateAdapter.getDateNames();
     this._weeks = [[]];
     for (let i = 0, cell = this._firstWeekOffset; i < daysInMonth; i++, cell++) {
@@ -101,8 +106,8 @@ export class MatCalendarMonthPerYearViewComponent implements OnInit {
         cell = 0;
       }
       const date = this.nativeDateAdapter.createDate(
-        this.nativeDateAdapter.getYear(this.currentDate),
-        this.nativeDateAdapter.getMonth(this.currentDate), i + 1);
+        this.nativeDateAdapter.getYear(this.selectedDate),
+        this.nativeDateAdapter.getMonth(this.selectedDate), i + 1);
       // const enabled = this._shouldEnableDate(date);
       // const ariaLabel = this.nativeDateAdapter.format(date, this._dateFormats.display.dateA11yLabel);
       this._weeks[this._weeks.length - 1]
