d5797e0b92aef1e91c7e04170e1415bea45bea92
Anthony Nahas <anthony.na@hotmail.de>
2018-11-12 11:33:09 +0100

    fix(package): fixed the year label in the toolbar calendar

--- src/module/component/mat-calendar.component.ts
+++ src/module/component/mat-calendar.component.ts
@@ -1,16 +1,44 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {MatCalendarService, MatCalendarView} from '../..';
+import {Subscription} from 'rxjs/internal/Subscription';
 
-export type MatCalendarView = 'yearly' | 'monthly' | 'weekly' | 'daily';
 
 @Component({
   selector: 'mat-calendar',
   templateUrl: './mat-calendar.component.html',
-  styleUrls: ['./mat-calendar.component.scss']
+  styleUrls: ['./mat-calendar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
-export class MatCalendarComponent {
+export class MatCalendarComponent implements OnInit, OnDestroy {
 
-  currentDate: Date = new Date();
+  currentDate: Date;
+
+  @Input()
   selectedDate: Date;
+
+  @Input()
   view: MatCalendarView = 'yearly';
+
+  $onViewChangedSubscription: Subscription;
+
+  constructor(public calendarService: MatCalendarService) {
+    this.currentDate = this.calendarService.currentDate;
+  }
+
+  ngOnInit(): void {
+
+    this.selectedDate ? this.calendarService.selectedDate = this.selectedDate : this.selectedDate = this.calendarService.selectedDate;
+
+    this.$onViewChangedSubscription = this.calendarService
+      .onViewChanged
+      .subscribe((view: MatCalendarView) => this.view = view);
+  }
+
+  ngOnDestroy(): void {
+    if (this.$onViewChangedSubscription) {
+      this.$onViewChangedSubscription.unsubscribe();
+    }
+  }
+
 }
 
