308135252e9413a818137f82c1110fe8f63b5ebf
Anthony Nahas <anthony.na@hotmail.de>
2018-11-13 11:53:05 +0100

    test(package): fixed the test configuration of the month per year component

--- src/module/component/mat-calendar-yearly-view/mat-calendar-month-per-year-view/mat-calendar-month-per-year-view.component.spec.ts
+++ src/module/component/mat-calendar-yearly-view/mat-calendar-month-per-year-view/mat-calendar-month-per-year-view.component.spec.ts
@@ -1,7 +1,17 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {MatCardModule, NativeDateAdapter} from '@angular/material';
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatCalendarBody,
+  MatCardModule,
+  MatDateFormats,
+  MatNativeDateModule,
+  NativeDateAdapter
+} from '@angular/material';
 import {MatCalendarMonthPerYearViewComponent} from './mat-calendar-month-per-year-view.component';
+import {MatCalendarService} from '../../../..';
+import {LOCALE_ID} from '@angular/core';
 
 describe('MatCalendarMonthPerYearViewComponent', () => {
   let component: MatCalendarMonthPerYearViewComponent;
@@ -9,15 +19,35 @@ describe('MatCalendarMonthPerYearViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatCardModule],
+      imports:
+        [
+          MatCardModule,
+          MatNativeDateModule
+        ],
       declarations:
         [
-          MatCalendarMonthPerYearViewComponent,
+          MatCalendarBody,
           MatCalendarMonthPerYearViewComponent
         ],
-      providers: [NativeDateAdapter]
-    })
-      .compileComponents();
+      providers:
+        [
+          MatCalendarService,
+          NativeDateAdapter,
+          {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+          {provide: LOCALE_ID, useValue: 'en-GB'},
+          {provide: MAT_DATE_FORMATS, useValue: {
+              parse: {
+                dateInput: 'D/MM/YYYY'
+              },
+              display: {
+                dateInput: 'DD/MM/YYYY',
+                monthYearLabel: 'MMMM Y',
+                dateA11yLabel: 'LL',
+                monthYearA11yLabel: 'MMMM Y'
+              }
+            }},
+        ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
