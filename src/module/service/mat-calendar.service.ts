44d80f50bdaf6dc0b4cf9ac201ea63d4f830a202
Anthony Nahas <anthony.na@hotmail.de>
2018-11-13 10:12:18 +0100

    feat(package): added reset method in the mat calendar service

--- src/module/service/mat-calendar.service.ts
+++ src/module/service/mat-calendar.service.ts
@@ -1,16 +1,26 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {MatCalendarView} from '../..';
+import * as _moment from 'moment';
+import {NativeDateAdapter} from '@angular/material';
+
+const moment = _moment;
 
 @Injectable()
 export class MatCalendarService {
 
   currentDate: Date = new Date();
-  selectedDate: Date = this.currentDate;
+  selectedDate: Date = new Date();
 
   // events
   onViewChanged: EventEmitter<MatCalendarView> = new EventEmitter<MatCalendarView>();
+  onDateReset: EventEmitter<Date> = new EventEmitter<Date>();
 
-  constructor() {
+  constructor(private _nativeDateAdapter: NativeDateAdapter) {
+  }
+
+  reset() {
+    this.selectedDate = this._nativeDateAdapter.clone(this.currentDate);
+    this.onDateReset.emit(this.selectedDate);
   }
 
   update(date: Date, view: MatCalendarView) {
