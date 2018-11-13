5525d442e1466f0e8f87bd904fa3cc464982398d
Anthony Nahas <anthony.na@hotmail.de>
2018-11-13 11:48:42 +0100

    test(package): provided NativeDateAdapter for the unit test

--- src/module/service/mat-calendar.service.spec.ts
+++ src/module/service/mat-calendar.service.spec.ts
@@ -1,11 +1,12 @@
 import {TestBed, inject} from '@angular/core/testing';
 
 import {MatCalendarService} from './mat-calendar.service';
+import {NativeDateAdapter} from '@angular/material';
 
 describe('MatCalendarService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MatCalendarService]
+      providers: [MatCalendarService, NativeDateAdapter]
     });
   });
 
