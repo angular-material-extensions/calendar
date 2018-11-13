e8805bf4cbf568073097c53da73141e1ea16025c
Anthony Nahas <anthony.na@hotmail.de>
2018-11-13 10:14:00 +0100

    fix(package): sync the year property with the mat calendar service

--- src/module/component/mat-calendar-yearly-view/mat-calendar-yearly-view.component.ts
+++ src/module/component/mat-calendar-yearly-view/mat-calendar-yearly-view.component.ts
@@ -1,32 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {MatCalendarService} from '../../..';
+import {Subscription} from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'mat-calendar-yearly-view',
   templateUrl: './mat-calendar-yearly-view.component.html',
   styleUrls: ['./mat-calendar-yearly-view.component.scss']
 })
-export class MatCalendarYearlyViewComponent implements OnInit {
+export class MatCalendarYearlyViewComponent implements OnInit, OnDestroy {
 
   selectedDate: Date;
   year: number;
+  $onDateResetSubscription: Subscription;
 
-  constructor(public calendarService: MatCalendarService) {
+  constructor(public calendarService: MatCalendarService,
+              private ref: ChangeDetectorRef) {
+    // this.ref.detach();
     this.selectedDate = this.calendarService.selectedDate;
     this.year = this.selectedDate.getFullYear();
-    console.log('this.selectedDate.getFullYear()', this.year);
   }
 
   ngOnInit() {
+    this.$onDateResetSubscription = this.calendarService
+      .onDateReset
+      .subscribe((date: Date) => this.resetYear());
+
+    // setInterval(() => {
+    //   console.log('round after 5s');
+    // }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.$onDateResetSubscription) {
+      this.$onDateResetSubscription.unsubscribe();
+    }
   }
 
   nextYear() {
     this.selectedDate.setFullYear(++this.year);
     console.log('next year: ', this.year);
+    console.log('selectedDate: ', this.selectedDate);
+    console.log('calendarService.selectedDate: ', this.calendarService.selectedDate);
+    this.ref.detectChanges();
   }
 
   previousYear() {
     this.selectedDate.setFullYear(--this.year);
     console.log('previous year: ', this.year);
+    this.ref.detectChanges();
   }
+
+  resetYear() {
+    this.selectedDate = this.calendarService.selectedDate;
+    this.year = this.selectedDate.getFullYear();
+  }
+
 }
