7efaa32e5cfc97a98755e60a16fe515a21f082a3
Anthony Nahas <anthony.na@hotmail.de>
2020-09-12 22:49:21 +0200

    feat(project): added support of angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule
   ],
